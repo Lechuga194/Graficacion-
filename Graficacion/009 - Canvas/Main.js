f82419fd71d189420423e6283aec495349a64312
Lechuga194 <joselechuga194@gmail.com>
2020-02-17 11:51:40 -0600

    asd

--- Graficacion/009 - Canvas/Main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function(evt) {
-  let canvas = document.getElementById("the_canvas");
-  let context = canvas.getContext("2d");
-  this.console.log("asd");
-});
-
-console.log(canvas);
-console.log(context);
